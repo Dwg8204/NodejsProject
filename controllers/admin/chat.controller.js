921f73bb675014e847a1e5e8d7dbbc9eca4f7f03
Dwg8204 <duongba0802@gmail.com>
2025-07-29 16:22:39 +0700

    lam tinh nang chat co ban

--- controllers/admin/chat.controller.js
+++ controllers/admin/chat.controller.js
@@ -1,11 +1,35 @@
+const Chat = require('../../models/chat.model');
+const account = require('../../models/account.model');
+
 module.exports.index = async (req, res) => {
-        _io.on('connection', (socket) => {
-        console.log('a user connected', socket.id);
-        socket.on('disconnect', () => {
-            console.log('user disconnected');
-        });
-    });
+    const userId = res.locals.account._id.toString();
+
+    // Lấy tất cả tin nhắn
+    const chats = await Chat.find({ deleted: false });
+
+    // Tìm user_id khác userId hiện tại (nick đối phương)
+    const opponentChat = chats.find(chat => chat.user_id.toString() !== userId);
+    let opponent = null;
+    if (opponentChat) {
+        opponent = await account.findOne({ _id: opponentChat.user_id }).select('fullName avatar');
+    }
+
+    // Gắn infoUser cho từng chat
+    for (const chat of chats) {
+        const infoUser = await account.findOne({ _id: chat.user_id }).select('fullName avatar');
+        chat.infoUser = infoUser;
+        chat.infoUser.userId = chat.user_id.toString();
+    }
+
     res.render('admin/pages/chat/index', {
-        pageTitle: 'Chat '
+        pageTitle: 'Chat',
+        chats: chats,
+        userId: userId,
+        opponent: opponent
+            ? {
+                fullName: opponent.fullName,
+                avatar: opponent.avatar || '/images/user-avatar.png'
+            }
+            : { fullName: '', avatar: '/images/user-avatar.png' }
     });
 };
--- public/admin/js/chat.js
+++ public/admin/js/chat.js
@@ -1,1 +1,15 @@
-var socket = io();
+const formSendData = document.querySelector('.inner-form');
+if(formSendData) {
+    formSendData.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const content = event.target.elements.content.value;
+        console.log(content);
+        if (content){
+            socket.emit('CLIENT_SEND_MESSAGE', content);
+            event.target.elements.content.value = ''; 
+        }
+    });
+}
+else {
+    console.error('Form with class "inner-form" not found.');
+}
